be51d01822d3c401921847916dd4f5a4576400a2
ZZHow <mail@zzhow.com>
2025-11-27 23:17:10 +0800

    chore: initialize backend project

---
+++ backend/src/test/java/com/zzhow/magicmibbackend/MagicMibBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzhow.magicmibbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicMibBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
